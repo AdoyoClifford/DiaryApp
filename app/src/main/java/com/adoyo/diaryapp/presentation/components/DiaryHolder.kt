3c8fd9eb416041976de38a91abd14ac43f51e017
Adoyo Clifford <adoyoclifford@gmail.com>
2023-06-20 12:02:06 +0300

    initial commit

--- app/src/main/java/com/adoyo/diaryapp/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/adoyo/diaryapp/presentation/components/DiaryHolder.kt
@@ -2,29 +2,92 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.adoyo.diaryapp.model.Diary
 import com.adoyo.diaryapp.model.Mood
+import com.adoyo.diaryapp.ui.theme.Elevation
+import com.adoyo.diaryapp.utils.toInstant
 import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.Date
 import java.util.Locale
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DiaryHolder(diary: Diary, onClick: (String) -> Unit) {
+    val localDensity = LocalDensity.current
+    var componentHeight by remember {
+        mutableStateOf(0.dp)
+    }
+    Row(
+        modifier = Modifier.padding(horizontal = 14.dp, vertical = 7.dp).clickable(
+            indication = null,
+            interactionSource = remember {
+                MutableInteractionSource()
+            }
+        ) { onClick(diary._id.toString()) }
+    ) {
+        Spacer(modifier = Modifier.width(14.dp))
+        Surface(
+            modifier = Modifier
+                .width(2.dp)
+                .height(componentHeight + 14.dp), tonalElevation = Elevation.Level1
+        ) {
+
+        }
+        Spacer(modifier = Modifier.width(20.dp))
+        Surface(
+            modifier = Modifier
+                .clip(shape = Shapes().medium)
+                .onGloballyPositioned {
+                    componentHeight = with(localDensity) { it.size.height.toDp() }
+                },
+            tonalElevation = Elevation.Level1
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                DiaryHeader(moodNme = diary.mood, time = diary.date.toInstant())
+                Text(
+                    text = diary.description,
+                    modifier = Modifier.padding(all = 14.dp),
+                    style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
+}
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -64,4 +127,16 @@ fun DiaryHeader(
             style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
         )
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun DiaryPreview() {
+    DiaryHolder(diary = Diary().apply {
+        title = "My Diary"
+        description =
+            "I can't believe it's 2023! This year is going to be amazing. I'm so excited to see what the future holds. I'm starting the year off with a clean slate, and I'm determined to make this year the best year yet."
+        mood = Mood.Happy.name
+    }, onClick = {})
 }
---
+++ app/src/main/java/com/adoyo/diaryapp/presentation/components/Gallery.kt
@@ -0,0 +1,90 @@
+package com.adoyo.diaryapp.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import kotlin.math.max
+
+@Composable
+fun Gallery(
+    modifier: Modifier = Modifier,
+    images: List<String>,
+    imagesSize: Dp = 40.dp,
+    spaceBetween: Dp = 10.dp,
+    imageShape: CornerBasedShape = Shapes().small
+) {
+    BoxWithConstraints {
+        val numberOfVisibleImages = remember {
+            derivedStateOf {
+                max(a = 0, b = this.maxWidth.div(imagesSize + spaceBetween).toInt().minus(1))
+            }
+
+        }
+        val remainingImages = remember {
+            derivedStateOf {
+                images.size - numberOfVisibleImages.value
+            }
+        }
+
+        Row {
+            images.take(numberOfVisibleImages.value).forEach { image ->
+                AsyncImage(
+                    modifier = Modifier
+                        .clip(imageShape)
+                        .size(imagesSize),
+                    model = ImageRequest.Builder(LocalContext.current).data(image).crossfade(true)
+                        .build(), contentDescription = "Gallery Image"
+                )
+                Spacer(modifier = Modifier.width(spaceBetween))
+            }
+        }
+    }
+}
+
+@Composable
+fun LastImageOverlay(
+    imageSize: Dp,
+    remainingImages: Int,
+    imageShape: CornerBasedShape
+) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Surface(
+            modifier = Modifier
+                .clip(imageShape)
+                .size(imageSize),
+            color = MaterialTheme.colorScheme.primaryContainer
+        ) {}
+        Text(
+            text = "+$remainingImages",
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Medium
+            ),
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+
+}
